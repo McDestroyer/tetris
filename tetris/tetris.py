770168d2b16b88465039229871086b7d3c633265
Jason <jasonfanger@hotmail.com>
2023-03-04 00:59:39 -0800

    Comments and keyboard input prototype added

--- tetris/tetris.py
+++ tetris/tetris.py
@@ -21,7 +21,8 @@ Desc: A generic function containing my default functions.
 
 
 # This is an importer I made for all of my programs going forward so I wouldn't have to deal with
-# creating and renaming the personal_functions.py and universal_colors.py for every program
+# creating and renaming the personal_functions.py and universal_colors.py for every program.
+# Probably adds unnecessary bulk here, but I don't care enough to make it reliable without it.
 import sys
 import os
 import time
@@ -30,40 +31,50 @@ try:
     from utilities import color
     from utilities import cursor
     from utilities import animations
+    from utilities import keyboard_input
     from utilities.personal_functions import *
 except ModuleNotFoundError:
     current = os.path.dirname(os.path.realpath(__file__))
 
-    while current.split("\\")[-1] != "src":
+    while current.split("\\")[-1] != "tetris":
         current = os.path.dirname(current)
     sys.path.append(current)
 
     from utilities import color
     from utilities import cursor
     from utilities import animations
+    from utilities import keyboard_input
     from utilities.personal_functions import *
 
 from tetris_functions import *
 
-
+# Formatting settings (screen size and offset from the left side of the screen so far)
 grid = (10, 24)
 LEFT_OFFSET = 5
 
+# Scores, levels, and life. Only life is implemented as of yet.
 score = 0
 high_scores = ["Name", "Score"]
 level = 1
 dead = False
 
-loop = 0
+# The delta time (how long each frame should take)
 delta_seconds = 1 / 60
 delta = delta_seconds / 1_000_000_000
 
+# The time between downward block movements. Scales inversely with level.
 g_time = (0.8 - ((level - 1) * 0.007)) ** (level - 1)
 previous_g = time.monotonic_ns()
 
+# The current loop and the loop on which to move down.
+loop = 0
+g_loop = rounder(g_time * (1 / delta_seconds))
+
+# The symbols used to represent a filled or empty square
 BS = "██"
 bl = "  "
 
+# The list of tetrominoes. The grids are shaped so they rotate correctly around the center.
 blocks = [
     [
         [bl, bl, bl, bl],
@@ -109,18 +120,20 @@ blocks = [
     ]
 ]
 
-# Columns in lines
+# This is the screen grid of squares and their colors
+# The outer list is the list of horizontal lines. The inner list is the relevant column entries.
+# Example: [0][0] is the bottom left, [4][3] is the 5th item up and the 4th item across.
 current_positions = [
         [
-            ["", color.DEFAULT_COLOR] for _ in range(grid[0])
+            ["  ", color.DEFAULT_COLOR] for _ in range(grid[0])
         ] for _ in range(grid[1])
     ]
 
 
 def _main() -> None:
     """Main"""
-    text("Welcome to our Tetris recreation!", mods=[color.UNDERLINE, color.GREET])
-    intext("Press Enter to start the game...")
+    text("Welcome to our Tetris recreation!\n", mods=[color.UNDERLINE, color.GREET])
+    intext("Press Enter to start the game...", mods=[color.CYAN])
 
     initialize()
 
@@ -130,39 +143,91 @@ def _main() -> None:
 
 def play():
     """Run the game."""
-    global loop
     start_time = time.monotonic_ns()
 
-
+    # Prints all blocks in all rotations as a test. It works.
+    # Left it as a visual representation of action occuring.
     for b in blocks:
         for i in range(4):
             print_block(rotate(b, i))
-    input()
+
+    # Gets keyboard commands and sends the summary of the actions requested to a list.
+    commands = listener()
+    print(commands)
 
 
+    # To be added:
+        # Check inputs and decide what needs to be done (keyboard listener WIP)
+        # Move blocks
+            # First rotate and move, then apply gravity
+            # Apply wallkick rules when applicable
+        # Clear lines and give points
+            # Add code for combos too
+        # Check if leveled up
+        # Check if dead
 
-    # Check inputs
-    # Move blocks
-    # Check lines
-    # Clear and give points
-    # Check if dead
-    # Wait for the delta
-    end_time = time.monotonic_ns()
-    real_delta = start_time - end_time
-    if real_delta < delta:
-        print(real_delta / delta)
-        pause_nanoseconds(delta - real_delta)
-    else:
-        print("OVERTIME")
-    loop += 1
-    if loop >= 60:
-        loop = 0
+        # Also maybe local multiplayer if I get really bored and we finish too early.
+
+
+    # Wait for the delta -- Done!
+    delta_wait(start_time, loop)
 
 
 def initialize():
     """Set up the screen and variables."""
-    # Greet
-    # Build "screen"
+    animations.loading_v3()
+    # Build "screen" frame
+
+
+def listener() -> list:
+    """Find out which commands need to be run. Could be later modified to run these commands.
+
+    Returns:
+        list: The lsit of commands to be followed.
+    """
+    commands = []
+    if keyboard_input.is_newly_pressed("a"):
+        commands.append("left")
+    if keyboard_input.is_newly_pressed("d"):
+        if "left" not in commands:
+            commands.append("right")
+        else:
+            commands.remove("left")
+
+    if keyboard_input.is_currently_pressed("s"):
+        commands.append("soft drop")
+    if keyboard_input.is_newly_pressed("w"):
+        commands.append("hard drop")
+
+    if keyboard_input.is_newly_pressed("j"):
+        commands.append("rotate left")
+    if keyboard_input.is_newly_pressed("l"):
+        if "rotate left" not in commands:
+            commands.append("rotate right")
+        else:
+            commands.remove("rotate left")
+
+    if keyboard_input.is_newly_pressed("i"):
+        commands.append("store")
+
+    return commands
+
+
+def delta_wait(start_time: int, current_loop: int):
+    """Pause for a short period to rate-limit the game and framerate.
+
+    Args:
+        start_time (int): The time at which the loop began.
+        loop (int): The current loop (used primarily for gravity)
+    """
+    end_time = time.monotonic_ns()
+    real_delta = start_time - end_time
+    if real_delta < delta:
+        pause_nanoseconds(delta - real_delta)
+    else:
+        print("OVERTIME")
+
+    current_loop += 1
 
 
 if __name__ == "__main__":
---
+++ tetris/utilities/keyboard_input.py
@@ -0,0 +1,85 @@
+"""Work with keyboard inputs."""
+# Disables annoying and usually incorrect warnings.
+# pylint: disable=wrong-import-position
+# pylint: disable=import-error
+
+import os
+import pkg_resources
+
+# Checks to see if the dependency is installed. If not, installs it.
+try:
+    pkg_resources.require("keyboard")
+except pkg_resources.DistributionNotFound:
+    os.system('pip install keyboard --quiet')
+
+import keyboard
+
+# The keys previously pressed
+keys = {}
+
+
+def is_newly_pressed(key: str, function: callable or None = None) -> bool:
+    """Detect if a key is pressed and return True if
+    it wasn't pressed the last time this function was called.
+    Designed to be run every frame.
+
+    Args:
+        key (str): 
+             key to check the newness of the compression thereof.
+        function (str, optional): 
+             The function to execute if the key is newly pressed.
+
+    Returns:
+        bool: True if the key is pressed but was not pressed during the previous call.
+        False otherwise.
+    """
+    result = False
+
+    # If the key has been pressed previously, check it's previous value.
+    # If it says it wasn't pressed, but it is now, change to fit and set the result accordingly
+    # and vise-versa.
+    # If the current and previous values are the same, set the result to False.
+    if key in keys:
+        if keyboard.is_pressed(key) and keys[key] is False:
+            keys[key] = True
+            result = True
+        elif keyboard.is_pressed(key) and keys[key] is True:
+            pass
+        else:
+            keys[key] = False
+
+    # If it isn't in the list, set the result and the value
+    # to whether or not it's currently pressed.
+    else:
+        if keyboard.is_pressed(key):
+            keys[key] = True
+            result = True
+        else:
+            keys[key] = False
+
+    # If a function is provided and the result was True, run the function.
+    if result and function is not None:
+        function()
+
+    return result
+
+def is_currently_pressed(key: str, function: callable or None = None) -> bool:
+    """Check to see if a key is currently pressed.
+
+    Args:
+        key (str): The key to check.
+        function (callable | None, optional):
+            The function to call if the key is pressed.
+            Defaults to None.
+
+    Returns:
+        bool: True if the key is pressed. False otherwise.
+    """
+    # Update the list
+    keys[key] = keyboard.is_pressed(key)
+
+    # Run if true
+    if function is not None and keys[key] is True:
+        function()
+
+    return keys[key]
